0046acfad6235b1950b740d99d1c3462e91425ac
Lucas Martins <lucas.martins.fsa@gmail.com>
2022-12-04 01:01:54 -0300

    [UPDATE] - organização da classe XbrainApplication

--- src/main/java/com/xbrain/XbrainApplication.java
+++ src/main/java/com/xbrain/XbrainApplication.java
@@ -3,23 +3,12 @@ package com.xbrain;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.xbrain.entities.Produto;
-
 @SpringBootApplication
 public class XbrainApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(XbrainApplication.class, args);
 		System.out.println("Servidor iniciado");
-
-		Produto produto = new Produto(1, "tenis", 20.0, 1);
-		System.out.println(produto);
-		produto.setId(2);
-		System.out.println(produto.getId());
-		System.out.println(produto);
-
-
-
 
 	}
 
